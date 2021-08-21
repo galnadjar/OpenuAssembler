981cee84269f7ac5ed389d0fad640d5a7e95f8a1
gal <galnadjar97@gmail.com>
2021-08-21 15:22:55 +0300

    completly working and orginized code before last test

---
+++ labelAnalyze.c
@@ -0,0 +1,102 @@
+
+#include "labelAnalyze.h"
+
+/*parses throguh the line of input looking for a label, if proper label sets the label variable and
+ * returns its ending index+1,
+ * otherwise return -1*/
+int analyzeLabel(char* lineInput, int i, int line, char** label){
+
+    int state = VALID,ch,wordStarted = 0,ind = 0;
+    char* word = (char*) calloc(MAX_LABEL_LENGTH+1,sizeof(char));
+
+    for(;i < strlen(lineInput) -1 && state == VALID;i++){
+        ch = (int)lineInput[i];
+
+        if(isspace(ch)){
+
+            if(!wordStarted)
+                continue;
+
+            else{
+                state = validLabelORInstruction(lineInput, LABEL_SEARCH);
+                if(state == ERROR)
+                    ERROR_LABEL_NAME(line);
+                else
+                    state = EXIT;
+            }
+        }
+
+        else if(isalpha(ch)){
+            if(ind == MAX_LABEL_LENGTH){
+                ERROR_LABEL_LENGTH(line);
+                state = ERROR;}
+
+            else{
+                wordStarted = 1;
+                word[ind++] = (char)ch;}
+        }
+
+        else if(wordStarted && isdigit(ch))
+            word[ind++] = (char)ch;
+
+
+        else{
+            ERROR_LABEL_NAME(line);
+            state = ERROR;
+        }
+    }
+
+    if(i == strlen(lineInput)-1 && !wordStarted){
+        ERROR_LABEL_NAME(line);
+        state = ERROR;}
+
+    if(state == ERROR)
+        i = ERROR;
+
+    else
+        strcpy((*label),word);
+
+    free(word);
+
+    return i;
+}
+
+
+/*this function checks if a valid label or instruction ,
+ * returns 1 if not found in the database otherwise if found returns 0*/
+int validLabelORInstruction(char* input, int request) {/*returns 1 if not found in the list, and 0 if found*/
+    int isFound = 0;
+    int bot = 0,top,mid;
+    int loc;
+
+    char* savedWords[] = {"add", "and", "andi", "asciz", "beq", "bgt", "blt", "bne", "call", "db", "dh", "dw", "entry",
+                          "extern", "jmp", "la", "lb", "lh", "lw", "move", "mvhi", "mvlo", "nor", "nori",
+                          "or", "ori", "sb", "sh", "stop", "sub", "subi", "sw"};
+
+    char* instructionWords[] = {"add", "and", "andi", "beq", "bgt", "blt", "bne", "call", "jmp", "la", "lb", "lh", "lw", "move",
+                                "mvhi", "mvlo", "nor", "nori", "or", "ori", "sb", "sh", "stop", "sub", "subi", "sw"};
+
+    if(!request)
+        top = SAVED_WORDS_LENGTH;
+
+    else /*means its an order*/
+        top = INSTRUCTION_WORDS_LENGTH;
+
+    while(top >= bot && !isFound){
+
+        mid = (top + bot) / 2;
+        loc = strcmp(input, (request ? instructionWords[mid] : savedWords[mid]));
+
+        if(loc == 0){
+            isFound = 1;
+        }
+
+        else if(loc < 0){
+            top = mid-1;
+        }
+
+        else
+            bot = mid+1;
+    }
+    return !isFound;
+}
